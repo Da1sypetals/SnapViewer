13edcf52f5bf45f33bfff9e11ae740aa05633b93
Da1sypetals <da1sypetals.iota@gmail.com>
2025-07-10 13:31:09 +0800

    refactor read_snap function to a large monolithic one

--- src/load.rs
+++ src/load.rs
@@ -6,21 +6,6 @@ use std::fs::File;
 use std::io::Read;
 use zip::ZipArchive;
 
-#[derive(Debug)]
-pub struct RawSnap {
-    /// Path to snapshot pickle file
-    pub(crate) path: String,
-    pub(crate) allocations: String,
-    pub(crate) elements: String,
-}
-
-/// Executed at start
-pub fn read_snap(zip_file_path: &str) -> anyhow::Result<Vec<Allocation>> {
-    info!("Loading snapshot...");
-    let rawsnap = read_snap_from_zip(zip_file_path)?;
-    load_allocations(rawsnap)
-}
-
 /// Unzips "allocations.json" and "elements.json" from a zip file into memory.
 ///
 /// ## Arguments
@@ -32,11 +17,11 @@ pub fn read_snap(zip_file_path: &str) -> anyhow::Result<Vec<Allocation>> {
 /// "elements.json", or an `io::Error` if an error occurs.
 ///
 /// Executed at start
-pub fn read_snap_from_zip(zip_file_path: &str) -> anyhow::Result<RawSnap> {
+pub fn read_snap(zip_file_path: &str) -> anyhow::Result<Vec<Allocation>> {
     info!("Loading json strings from zip...");
 
-    let mut allocations: Option<String> = None;
-    let mut elements: Option<String> = None;
+    let mut raw_allocs: Vec<RawAllocationData> = Vec::new();
+    let mut elements: Vec<ElementData> = Vec::new();
 
     // Open the zip file
     let file = File::open(zip_file_path)?;
@@ -62,79 +47,58 @@ pub fn read_snap_from_zip(zip_file_path: &str) -> anyhow::Result<RawSnap> {
 
                 let mut content = String::new();
                 file.read_to_string(&mut content)?;
-                allocations = Some(content);
 
                 bar.finish();
                 println!("Memory after loading allocs: {} MiB", memory_usage());
+
+                let bar = get_spinner("Parsing json to data structure...")?;
+
+                raw_allocs = serde_json::from_str(&content).map_err(|e| {
+                    anyhow::anyhow!(
+                        "Failed to parse allocations JSON from '{:?}': {}",
+                        zip_file_path,
+                        e
+                    )
+                })?;
+                println!("Memory after deserializing allocs: {} MiB", memory_usage());
+
+                bar.finish();
             } else if filename == ELEMENTS_FILE_NAME {
                 info!("Reading {} to string", ELEMENTS_FILE_NAME);
                 let bar = get_spinner(&format!("Reading {} to string", ELEMENTS_FILE_NAME))?;
 
                 let mut content = String::new();
                 file.read_to_string(&mut content)?;
-                elements = Some(content);
 
                 bar.finish();
                 println!("Memory after loading elems: {} MiB", memory_usage());
+
+                elements = serde_json::from_str(&content).map_err(|e| {
+                    anyhow::anyhow!(
+                        "Failed to parse elements JSON from '{:?}': {}",
+                        zip_file_path,
+                        e
+                    )
+                })?;
+                println!(
+                    "Memory after deserializing elements: {} MiB",
+                    memory_usage()
+                );
             }
         }
     }
 
-    match (allocations, elements) {
-        (None, None) => Err(anyhow::anyhow!("allocations and elements not found!")),
-        (None, Some(_)) => Err(anyhow::anyhow!("allocations not found!")),
-        (Some(_), None) => Err(anyhow::anyhow!("elements not found!")),
-        (Some(allocs), Some(elems)) => Ok(RawSnap {
-            path: zip_file_path.into(),
-            allocations: allocs,
-            elements: elems,
-        }),
-    }
-}
-
-/// Executed at start
-pub fn load_allocations(rawsnap: RawSnap) -> anyhow::Result<Vec<Allocation>> {
-    info!("Parsing json to data structure...");
-    let bar = get_spinner("Parsing json to data structure...")?;
-
-    let raw_allocs: Vec<RawAllocationData> =
-        serde_json::from_str(&rawsnap.allocations).map_err(|e| {
-            anyhow::anyhow!(
-                "Failed to parse allocations JSON from '{:?}': {}",
-                rawsnap.path,
-                e
-            )
-        })?;
-    println!("Memory after deserializing allocs: {} MiB", memory_usage());
-
-    // elements.json is a list, where each item has a "frames" key.
-    let elements_data: Vec<ElementData> = serde_json::from_str(&rawsnap.elements).map_err(|e| {
-        anyhow::anyhow!(
-            "Failed to parse elements JSON from '{:?}': {}",
-            rawsnap.path,
-            e
-        )
-    })?;
-    bar.finish();
-    println!(
-        "Memory after deserializing elements: {} MiB",
-        memory_usage()
-    );
-
-    // Check if the number of allocations matches the number of element data (callstacks)
-    if raw_allocs.len() != elements_data.len() {
+    if raw_allocs.len() != elements.len() || raw_allocs.is_empty() {
         return Err(anyhow::anyhow!(
-            "Mismatch in the number of entries: {} allocations vs {} elements",
+            "Mismatch in the number of entries (required non-empty equal): {} allocations vs {} elements",
             raw_allocs.len(),
-            elements_data.len()
+            elements.len()
         ));
     }
 
-    // Combine the data from raw_allocs and elements_data (callstacks)
-    info!("Combining allocations and elements data...");
     let allocations: Vec<Allocation> = raw_allocs
         .into_iter()
-        .zip(elements_data)
+        .zip(elements)
         .map(|(raw_alloc, element_data)| {
             let peak_base = *raw_alloc.offsets.iter().max().unwrap();
             let peak_timestamps = raw_alloc
@@ -163,10 +127,6 @@ pub fn load_allocations(rawsnap: RawSnap) -> anyhow::Result<Vec<Allocation>> {
         })
         .progress()
         .collect();
-    println!(
-        "Memory after combining allocation data: {} MiB",
-        memory_usage()
-    );
 
     Ok(allocations)
 }
